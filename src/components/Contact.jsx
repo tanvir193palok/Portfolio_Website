58c1251ddd4ee4aada43ae736614d397051ed40a
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-11-10 21:03:48 +0600

    Contact Component Created

---
+++ src/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-full md:h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl  font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <form
+            action="https://getform.io/f/b1be36f6-5358-41f9-a47f-66dac8555815"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Your Name"
+              className="p-2 bg-transparent rounded-md border-2 text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter Your Email"
+              className="p-2 my-4 bg-transparent rounded-md border-2 text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter Your Message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's Talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
