9d8bff2b097e28a510138bd090e48827ed8c66ce
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-11-10 22:39:45 +0600

    Updated Components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import myImage from "../assets/Polock.jpeg";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="h-full md:h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl  sm:text-7xl font-bold text-white">
-            I am Junior Frontend Developer
+            I am a Frontend Developer
           </h2>
-          <p className="text-gray-500 py-4 mx-w-md">
+          <p className="text-gray-500 mr-4 py-4 mx-w-md">
             Certainly! Here's a sample text for your frontend developer about .
             Whether it's leveraging React for dynamic user interfaces or
             optimizing performance with efficient code, I thrive on turning
@@ -21,12 +22,17 @@ const Home = () => {
             digital vision to reality! 🌐✨
           </p>
           <div>
-            <button className="text-white group w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="text-white group w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdKeyboardDoubleArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,50 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import Project1 from "../assets/portfolio/Project-1.png";
+import Project2 from "../assets/portfolio/Project-2.png";
+import Project3 from "../assets/portfolio/Project-3.png";
+import Project4 from "../assets/portfolio/Project-4.png";
+import Project5 from "../assets/portfolio/Project-5.png";
+import Project6 from "../assets/portfolio/Project-6.png";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: Project1,
+      liveUrl: "https://tanvir193palok.github.io/E-commerce-Site/",
+      gitUrl: "https://github.com/tanvir193palok/E-commerce-Site",
     },
     {
       id: 2,
-      src: reactParallax,
+      src: Project2,
+      liveUrl: "https://tanvir193palok.github.io/login",
+      gitUrl: "https://github.com/tanvir193palok/Quiz-Application",
     },
     {
       id: 3,
-      src: navbar,
+      src: Project3,
+      liveUrl:
+        "https://653ebc0b7cf24033aad61f55--deluxe-kitten-e4a77b.netlify.app/",
+      gitUrl: "https://github.com/tanvir193palok/ThreeJS-Project",
     },
     {
       id: 4,
-      src: reactSmooth,
+      src: Project4,
+      liveUrl: "https://tanvir193palok.github.io/MaterialUI-Project/",
+      gitUrl: "https://github.com/tanvir193palok/MaterialUI-Project",
     },
     {
       id: 5,
-      src: installNode,
+      src: Project5,
+      liveUrl: "https://symphonious-genie-d5fd8f.netlify.app/",
+      gitUrl:
+        "https://github.com/tanvir193palok/Summarizer/tree/main/ai_summarizer",
     },
     {
       id: 6,
-      src: reactWeather,
+      src: Project6,
+      liveUrl: "https://chat-gpt-weld-three.vercel.app/",
+      gitUrl: "https://github.com/tanvir193palok/ChatGPT",
     },
   ];
 
@@ -48,7 +62,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, liveUrl, gitUrl }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -56,10 +70,16 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => (window.location.href = liveUrl)}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => (window.location.href = gitUrl)}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Code
                 </button>
               </div>
