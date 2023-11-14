bf7556db87638198ec1f9958bfa5871fb4ec2da4
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-11-15 04:07:52 +0600

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-full md:h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="h-screen md:h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex mr-4 flex-col justify-center h-full">
