346ea52df2b5670a90690c7c7bcd6288b437359b
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-01-22 23:10:51 +0600

    Experience section updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,54 +48,72 @@ const Experience = () => {
       src: html,
       title: "HTML",
       style: "shadow-orange-500",
+      text: "text-orange-500",
+      experience: "3+",
     },
     {
       id: 2,
       src: css,
       title: "CSS",
       style: "shadow-blue-500",
+      text: "text-blue-500",
+      experience: "3+",
     },
     {
       id: 3,
       src: javascript,
       title: "JavaScript",
       style: "shadow-yellow-500",
+      text: "text-yellow-500",
+      experience: "3+",
     },
     {
       id: 4,
       src: reactImage,
       title: "React",
       style: "shadow-blue-600",
+      text: "text-blue-600",
+      experience: "2+",
     },
     {
       id: 5,
       src: tailwind,
       title: "Tailwind",
       style: "shadow-sky-400",
+      text: "text-sky-400",
+      experience: "2+",
     },
     {
       id: 6,
       src: java,
       title: "Java",
       style: "shadow-rose-500",
+      text: "text-rose-500",
+      experience: "2+",
     },
     {
       id: 7,
       src: python,
       title: "Python",
       style: "shadow-[#2E8A99]",
+      text: "text-[#2E8A99]",
+      experience: "4+",
     },
     {
       id: 8,
       src: tensorflow,
       title: "Tensorflow",
       style: "shadow-[#F05941]",
+      text: "text-[#F05941]",
+      experience: "2+",
     },
     {
       id: 9,
       src: github,
       title: "GitHub",
       style: "shadow-gray-400",
+      text: "text-gray-400",
+      experience: "2+",
     },
   ];
 
@@ -115,7 +133,7 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="py-8 text-xl">Following are my tech stack</p>
+          <p className="py-8 text-xl">Following are my tech stack with years of experience.</p>
         </motion.div>
 
         <motion.div
@@ -123,14 +141,20 @@ const Experience = () => {
           initial="initial"
           animate={isInView && "final"}
           ref={ref}
-          className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0"
+          className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 sm:px-0"
         >
-          {techs.map(({ id, src, title, style }) => (
+          {techs.map(({ id, src, title, style, text, experience }) => (
             <motion.div
               variants={variants}
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
+              <div
+                className={`flex items-center justify-end pr-2 md:pr-5 text-z-50 ${text}`}
+              >
+                <span className="hidden md:inline text-bold text-lg">{experience} years</span>
+                <span className="md:hidden">{experience}</span>
+              </div>
               <img src={src} alt="experience" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </motion.div>
